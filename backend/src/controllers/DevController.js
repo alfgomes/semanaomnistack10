d508e19aa90fc68811e7dd8ea055618942ae161f
André Gomes <alfgomes@outlook.com>
2020-01-21 12:26:59 +0000

    Adicionando os métodos do exercício

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -39,5 +39,44 @@ module.exports = {
         };
     
         return response.json(dev);
-    }
+    },
+
+    async update(request, response) {
+        const { github_username } = request.query;
+        const { name, avatar_url, bio, techs, longitude, latitude } = request.body;
+
+        const techsArray = parseStringAsArray(techs);
+        
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        };
+        
+        let dev = await Dev.findOne( { github_username } );
+        if (dev) {
+            dev.name = name != null ? name : dev.name,
+            dev.avatar_url = avatar_url != null ? avatar_url : dev.avatar_url,
+            dev.bio = bio != null ? bio : dev.bio,
+            dev.techs = techsArray != null ? techsArray : dev.techs,
+            dev.location = location != null ? location : dev.location;
+
+            const dbRet = await Dev.update(dev);
+            
+            return response.json(dev);
+        } else {
+            return response.status(404).json({ message: 'Não foi encontrado registro para alteração.' });
+        };
+    },
+
+    async destroy(request, response) {
+        const { github_username } = request.query;
+
+        let dev = await Dev.findOne( { github_username } );
+        if (dev) {
+            //Dev.deleteOne(dev);
+            return response.json({});
+        } else {
+            return response.status(404).json({ message: 'Não foi encontrado registro para deleção.' });
+        };
+    },
 };
